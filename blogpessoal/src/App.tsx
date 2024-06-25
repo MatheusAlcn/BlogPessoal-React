f3d8ca62aa354f06369f09c5f390e908483277e7
MatheusAlcn <matheusalcantara717@gmail.com>
2024-06-25 19:28:55 -0300

    Criando as rotas

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,14 +1,26 @@
-import Home from './assets/paginas/home/Home';
-import './App.css';
 import React from 'react';
+import './App.css';
+import Home from './assets/paginas/home/Home';
+import Navbar from './NavBar/Navbar';
+import Footer from './components/footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Login/Login';
 
 function App() {
- 
   return (
     <>
-      <Home/>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
-  );
+);
 }
-
 export default App;
