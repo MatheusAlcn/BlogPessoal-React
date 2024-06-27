712ca130557f652f5608b0db833d7fc43af136d4
MatheusAlcn <matheusalcantara717@gmail.com>
2024-06-27 16:09:17 -0300

    Construindo Model, Context e Service do Cadastro de Usuários

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import './App.css';
-import Home from './assets/paginas/home/Home';
+
 import Navbar from './NavBar/Navbar';
 import Footer from './components/footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './Login/Login';
+import Cadastro from './Cadastro/Cadastro';
+import Home from './assets/paginas/home/Home';
+import { AuthProvider } from './Context/AuthContext';
+
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-        <Navbar />
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+        </AuthProvider>
     </>
-);
+  );
 }
 export default App;
